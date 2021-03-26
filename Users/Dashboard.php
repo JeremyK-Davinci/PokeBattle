dd7787a8f0172de79581624278378f5cb259d20f
JeremyK <99058701@mydacinci.nl>
2021-03-26 23:07:37 +0100

    Added a "working" fix for overlay
    While the current implementation of the overlay works, it feels wrong, room for improvement

--- Users/Dashboard.php
+++ Users/Dashboard.php
@@ -20,15 +20,15 @@
             <div class="border border-bottom-0 border-left-0 border-right-0 border-light mb-3">
                 <p class="border rounded bg-danger text-light text-center responseFail mt-3"></p>
                 <p class="border rounded bg-success text-light text-center responseSuccess mt-3"></p>
-                <div class="row-username d-flex flex-xxl-row flex-column mt-3 ml-xl-5">
+                <div class="row-username d-flex flex-xxl-row flex-column mt-3">
                     <label for="username" class="text-light flex-column h3">Username : </label>
                     <input type="text" class="form-control col-8 offset-1 flex-column updateName" name="username" value=<?= $_SESSION['username']?>>
                 </div>
-                <div class="row-mail d-flex flex-xxl-row flex-column mt-3 ml-xl-5">
+                <div class="row-mail d-flex flex-xxl-row flex-column mt-3">
                     <label for="mail" class="text-light flex-column h3 pr-5">E-Mail : </label>
                     <input type="text" class="form-control col-8 offset-1 flex-column updateMail" name="mail" value=<?= $_SESSION['mail']?>>
                 </div>
-                <div class="row-password d-flex flex-xxl-row flex-column mt-3 ml-xl-5">
+                <div class="row-password d-flex flex-xxl-row flex-column mt-3">
                     <label for="password" class="text-light flex-column h3 pr-2-o">Password : </label>
                     <input type="password" class="form-control col-8 offset-1 flex-column updatePass" name="password" value="########">
                 </div>
